2f68c610b3d57dbd2eba68a3f52bd8db540fcdb0
Kristoffer Moberg Christensen <kristofferMoberg.christensen@skatteetaten.no>
2018-12-19 16:26:03 +0100

    jsonNode extension function

---
+++ src/main/kotlin/no/skatteetaten/aurora/cantus/extensions/JsonNode.kt
@@ -0,0 +1,17 @@
+package no.skatteetaten.aurora.cantus.extensions
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+
+fun JsonNode?.asMap(): Map<String, JsonNode> {
+    if (this == null) {
+        return emptyMap()
+    }
+
+    if (this is ObjectNode) {
+        val fields = java.util.HashMap<String, JsonNode>()
+        this.fields().forEachRemaining { entry -> fields.put(entry.key, entry.value) }
+        return fields
+    }
+    return emptyMap()
+}
