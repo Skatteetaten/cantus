439ca9fa9ff128c0dc144d3c6f6bf9a697188007
Kristoffer Moberg Christensen <kristofferMoberg.christensen@skatteetaten.no>
2019-01-03 11:00:32 +0100

    Removed SourceSystemException

--- src/main/kotlin/no/skatteetaten/aurora/cantus/SourceSystemException.kt
+++
@@ -1,3 +0,0 @@
-package no.skatteetaten.aurora.cantus
-
-open class SourceSystemException(message: String, cause: Throwable? = null, code: String = "", errorMessage: String = message) : RuntimeException(message, cause)
