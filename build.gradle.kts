d594cb8a567402c0e1874e858de61be0d987c688
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-06-26 09:30:42 +0200

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,34 @@
 plugins {
     id("org.springframework.cloud.contract")
-    id("org.jetbrains.kotlin.jvm") version "1.3.21"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
-    id("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+    id("org.jetbrains.kotlin.jvm") version "1.3.40"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.40"
+    id("org.jlleitschuh.gradle.ktlint") version "8.1.0"
 
-    id("org.springframework.boot") version "2.1.3.RELEASE"
-    id("org.asciidoctor.convert") version "1.6.0"
+    id("org.springframework.boot") version "2.1.6.RELEASE"
+    id("org.asciidoctor.convert") version "2.2.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
     id("com.github.ben-manes.versions") version "0.21.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.9"
+    id("se.patrikerdes.use-latest-versions") version "0.2.10"
 
-    id("no.skatteetaten.gradle.aurora") version "2.0.2"
+    id("no.skatteetaten.gradle.aurora") version "2.3.1"
 
 }
 
 dependencies {
     implementation("uk.q3c.rest:hal-kotlin:0.5.4.0.db32476")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.1.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.2")
 
-    testImplementation("com.squareup.okhttp3:mockwebserver:3.13.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:3.14.2")
 
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("io.projectreactor.addons:reactor-extra:3.2.3.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    testImplementation("io.mockk:mockk:1.9.2")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.13")
+    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.17")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
-    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:0.6.2")
+    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:0.6.5")
 }
