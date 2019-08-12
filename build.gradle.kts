7417dd9544b8fab6aea7a2832c2c0154592a9187
Erlend Haugsnes <Erlend.Haugsnes@skatteetaten.no>
2019-08-12 12:39:44 +0200

    updated more plugins. Fixed builderrors

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.22.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.12"
 
-    id("no.skatteetaten.gradle.aurora") version "local-SNAPSHOT"
+    id("no.skatteetaten.gradle.aurora") version "2.4.2"
 }
 
 dependencies {
