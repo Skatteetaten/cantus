9df642d3dd827b763995aab65e8f451db683a0b9
Kristoffer Moberg Christensen <kristoffermoberg.christensen@skatteetaten.no>
2020-08-17 14:25:04 +0200

    AOS-4616: Fix dependency upgrade problems

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
     id("org.sonarqube") version "3.0"
 
-    id("org.springframework.boot") version "2.3.2.RELEASE"
+    id("org.springframework.boot") version "2.3.3.RELEASE"
 
     id("com.gorylenko.gradle-git-properties") version "2.2.2"
     id("com.github.ben-manes.versions") version "0.29.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.14"
 
-    id("no.skatteetaten.gradle.aurora") version "3.6.4"
+    id("no.skatteetaten.gradle.aurora") version "3.6.5"
 }
 
 dependencies {
@@ -24,14 +24,14 @@ dependencies {
     implementation("io.projectreactor.addons:reactor-extra:3.3.3.RELEASE")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.3.8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
 
     // Testing
     testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.0")
     testImplementation("io.mockk:mockk:1.10.0")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.22")
-    testImplementation("com.ninja-squad:springmockk:2.0.2")
+    testImplementation("com.ninja-squad:springmockk:2.0.3")
 
     // Spring
     testImplementation("org.springframework.boot:spring-boot-starter-test")
