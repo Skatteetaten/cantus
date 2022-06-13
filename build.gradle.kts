98c00bc4ac5208d5492d4e99f0e2da7c282d058a
Jarle Hansen <jarle@jarlehansen.net>
2022-06-13 14:28:11 +0200

    bump versions (#49)
    * bump versions
    
    * update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("no.skatteetaten.gradle.aurora") version "4.4.15"
+    id("no.skatteetaten.gradle.aurora") version "4.4.22"
 }
 
 aurora {
@@ -15,18 +15,18 @@ dependencies {
 
     implementation("uk.q3c.rest:hal-kotlin:0.5.4.0.db32476")
 
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.6")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
 
     // Testing
     testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.7")
-    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.2.0")
-    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.3.1")
+    testImplementation("io.mockk:mockk:1.12.4")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
-    testImplementation("com.ninja-squad:springmockk:3.1.0")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
 
     // Spring
     testImplementation("org.springframework.boot:spring-boot-starter-test")
